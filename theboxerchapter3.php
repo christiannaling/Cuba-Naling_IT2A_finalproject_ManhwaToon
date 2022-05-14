046a9190c85dd3b6e77efd6f718c0bd1569f280c
christiannaling <christiannaling4@gmail.com>
2022-05-14 17:22:10 +0800

    project script 14

---
+++ theboxerchapter3.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Project</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="the_boxer.php">Chapter List</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="content.php">Content</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="aboutus.php">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="contactus.php">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Read on ManhwaToon</h1>
+                <p class="lead"> Enjoy reading your favourite Manhwa online.</p>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="Manhwa">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                          <h1 class="fw-bolder">Chapter 3</h1>
+                        <ul type="none">
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/001.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/002.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/003.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/004.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/005.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/006.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/007.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/008.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/009.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/010.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/011.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/012.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/013.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/014.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/015.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/016.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/017.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/018.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/019.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/020.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/021.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/022.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/023.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/024.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/025.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/026.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/027.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/028.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/029.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/030.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/031.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/032.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/033.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/034.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/035.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/036.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/037.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/038.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/039.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/040.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/041.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/042.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/043.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/044.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/045.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/046.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/047.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/048.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/049.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/050.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/051.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                            <li><img src="images/the_boxer/_Ep. 3 - The Build-Up Ch. 3/052.jpg" alt="" class="img-fluid rounded shadow-sm"></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. ManhwaToon</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
